37b447d821a25e61163d0336be34de9eee353cb1
Filip Knezevic <filipknezevic369@gmail.com>
2025-10-15 00:08:27 +0200

    Napravljeni HTML i SCSS fajlovi za pregled knjiga i model knjige u JS

---
+++ library/app/pregled/pregled.js
@@ -0,0 +1,12 @@
+'use strict';
+
+class Knjiga {
+    constructor(id, naziv, datumStampanja, url, opis, popularnost) {
+        this.id = id;
+        this.naziv = naziv;
+        this.datumStampanja = datumStampanja;
+        this.url = url;
+        this.opis = opis;
+        this.popularnost = popularnost;
+    }
+}
